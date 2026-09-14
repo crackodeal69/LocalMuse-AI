b8e4be18cc75758b1e721d391806f11a816f4090
Oleksandr <zxcedition2604@gmail.com>
2026-09-14 05:50:17 +0300

    Pin Forge API to dedicated port

--- scripts/start_forge_api.bat
+++ scripts/start_forge_api.bat
@@ -4,5 +4,5 @@ setlocal
 if not defined LOCALMUSE_FORGE_ROOT set "LOCALMUSE_FORGE_ROOT=E:\ai_work\webui"
 if exist "%LOCALMUSE_FORGE_ROOT%\..\environment.bat" call "%LOCALMUSE_FORGE_ROOT%\..\environment.bat"
 cd /d "%LOCALMUSE_FORGE_ROOT%"
-set COMMANDLINE_ARGS=--api --nowebui
+set COMMANDLINE_ARGS=--api --nowebui --port 7860
 call webui.bat
